1222943011249694b08a614e1d306f5582e4c626
Prajyot <prajyotm90@gmail.com>
2020-09-05 14:04:10 +0530

    Adding FirstJavaClass and DataTypes classes

---
+++ BasicCoreJava/src/variablesAndDataTypes/DataTypes.java
@@ -0,0 +1,24 @@
+package variablesAndDataTypes;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int age = 29;
+		String name = "Prajyot"; //String is not a datatype it is a class
+		char gender = 'M';
+		boolean maritalStatus = true;
+		double height = 5.7;
+		
+		//data types primitive & non primitive
+		//rpimitive- int, char, double, byte, short, long, float, boolean
+		//primitive data types has no reference they are predefined
+		
+		//non-primitive- classes, collections, interfaces, arrays
+		
+		System.out.println(age);
+
+	}
+
+}
---
+++ BasicCoreJava/src/variablesAndDataTypes/FirstJavaClass.java
@@ -0,0 +1,8 @@
+package variablesAndDataTypes;
+
+public class FirstJavaClass {
+	public static void main(String args[]) {
+		System.out.println("First Java Class");
+	}
+
+}
