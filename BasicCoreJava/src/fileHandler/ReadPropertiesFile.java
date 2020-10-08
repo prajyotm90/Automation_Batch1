3112455f7b4f7ca1ffd774c075e7b59f63f89afa
Prajyot <prajyotm90@gmail.com>
2020-10-08 13:43:33 +0530

    Pushing changes for filehandler and testng and cucumber and postman

--- BasicCoreJava/src/fileHandler/ReadPropertiesFile.java
+++ BasicCoreJava/src/fileHandler/ReadPropertiesFile.java
@@ -1,10 +1,28 @@
 package fileHandler;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ReadPropertiesFile {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println(System.getProperty("user.dir"));
 
+		String filepath = System.getProperty("user.dir") + "\\src\\fileHandler\\TestData1.properties";
+		// D:\Automation_Batch1\Automation_Batch1\BasicCoreJava\src\fileHandler\TestData.properties
+
+		System.out.println(filepath);
+
+		FileInputStream file = new FileInputStream(filepath);
+
+		Properties p = new Properties();
+		p.load(file);
+
+		System.out.println(p.getProperty("name"));
+		System.out.println(p.getProperty("Age"));
+		System.out.println(p.getProperty("surname"));
 	}
 
 }
